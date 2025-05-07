a008fc966dd4f1d699ceeaf51116461641f7d630
xinyanguan <xinyanguan@tencent.com>
2025-05-07 19:36:44 +0800

    train script

---
+++ scripts/nq_hotpotqa/v0.2/train_deeprag_grpo.sh
@@ -0,0 +1,97 @@
+# # 全局配置优先级高，默认配置文件存放在/root/.config/pip/pip.conf
+# pip config set global.index-url https://mirrors.tencent.com/pypi/simple/
+# pip config set global.extra-index-url https://mirrors.tencent.com/repository/pypi/tencent_pypi/simple
+# pip config set global.trusted-host mirrors.tencent.com
+
+# pip install beir
+# pip install elasticsearch==7.17.9
+# pip install -e /mnt/geminiszgmcephfs/geminicephfs/pr-others-prctrans/xinyanguan/Search-R1
+# pip install nvidia-cublas-cu12==12.4.5.8
+
+
+data_name=nq_hotpotqa_train
+
+export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
+export DATA_DIR=data/${data_name} # first download the data from https://huggingface.co/datasets/PeterJinGo/nq_hotpotqa_train
+
+WAND_PROJECT="Search-R1"
+
+# export BASE_MODEL='Qwen/Qwen2.5-3B'
+# export EXPERIMENT_NAME=${train_data}-${test_data}-search-r1-grpo-qwen2.5-3b-em
+# export BASE_MODEL='Qwen/Qwen2.5-3B-Instruct'
+# export EXPERIMENT_NAME=${train_data}-${test_data}-search-r1-grpo-qwen2.5-3b-it-em
+# export BASE_MODEL='Qwen/Qwen2.5-7B'
+# export BASE_MODEL='/mnt/geminiszgmcephfs/geminicephfs/pr-others-prctrans/xinyanguan/hf_models/Qwen2.5-7B-Instruct'
+export BASE_MODEL='/mnt/geminiszgmcephfs/geminicephfs/pr-others-prctrans/xinyanguan/deeprag_checkpoint/llama-sft/hotpot-wikihop-full'
+export EXPERIMENT_NAME=deeprag-grpo-train
+# export BASE_MODEL='Qwen/Qwen2.5-7B-Instruct'
+# export EXPERIMENT_NAME=${train_data}-${test_data}-search-r1-grpo-qwen2.5-7b-it-em
+# export BASE_MODEL='Qwen/Qwen2.5-14B'
+# export EXPERIMENT_NAME=${train_data}-${test_data}-search-r1-grpo-qwen2.5-14b-em
+# export BASE_MODEL='Qwen/Qwen2.5-14B-Instruct'
+# export EXPERIMENT_NAME=${train_data}-${test_data}-search-r1-grpo-qwen2.5-14b-it-em
+
+# set -x
+export VLLM_ATTENTION_BACKEND=XFORMERS # vllm + qwen2-7b with flash_attn has some issues
+export RAY_DEBUG=legacy
+
+# max_prompt_length = (config['training']['max_start_length'] + config['training']['max_response_length'] * (config['training']['max_turns'] - 1) + config['training']['max_obs_length'] * config['training']['max_turns'])
+
+export ES_HOST=30.246.242.73
+
+wandb offline
+ray stop
+PYTHONUNBUFFERED=1 python3 -m verl.trainer.main_ppo \
+    data.train_files=/mnt/geminiszgmcephfs/geminicephfs/pr-others-prctrans/xinyanguan/Search-R1/data/deeprag/train.parquet \
+    data.val_files=/mnt/geminiszgmcephfs/geminicephfs/pr-others-prctrans/xinyanguan/Search-R1/data/hotpot-dev-1k/train.parquet \
+    data.train_data_num=null \
+    data.val_data_num=null \
+    data.train_batch_size=32 \
+    data.val_batch_size=256 \
+    data.max_prompt_length=4096 \
+    data.max_response_length=4096 \
+    data.max_start_length=2048 \
+    data.max_obs_length=4096 \
+    data.shuffle_train_dataloader=True \
+    algorithm.adv_estimator=grpo \
+    actor_rollout_ref.model.path=$BASE_MODEL \
+    actor_rollout_ref.model.enable_gradient_checkpointing=true \
+    actor_rollout_ref.model.use_remove_padding=True \
+    actor_rollout_ref.actor.optim.lr=1e-6 \
+    actor_rollout_ref.actor.optim.lr_warmup_steps_ratio=0.285 \
+    actor_rollout_ref.actor.use_kl_loss=true \
+    actor_rollout_ref.actor.ppo_mini_batch_size=32 \
+    actor_rollout_ref.actor.ppo_micro_batch_size=8 \
+    actor_rollout_ref.actor.fsdp_config.param_offload=false \
+    actor_rollout_ref.actor.fsdp_config.grad_offload=false \
+    actor_rollout_ref.actor.fsdp_config.optimizer_offload=false \
+    actor_rollout_ref.rollout.log_prob_micro_batch_size=8 \
+    actor_rollout_ref.rollout.tensor_model_parallel_size=4 \
+    actor_rollout_ref.rollout.name=vllm \
+    actor_rollout_ref.rollout.gpu_memory_utilization=0.6 \
+    actor_rollout_ref.ref.log_prob_micro_batch_size=8 \
+    actor_rollout_ref.ref.fsdp_config.param_offload=True \
+    actor_rollout_ref.actor.kl_loss_coef=0.001 \
+    actor_rollout_ref.actor.kl_loss_type=low_var_kl \
+    algorithm.no_think_rl=false \
+    actor_rollout_ref.rollout.n_agent=8 \
+    actor_rollout_ref.rollout.temperature=1 \
+    actor_rollout_ref.actor.state_masking=true \
+    trainer.logger=['wandb','console'] \
+    +trainer.val_only=false \
+    +trainer.val_before_train=false \
+    trainer.default_hdfs_dir=null \
+    trainer.n_gpus_per_node=8 \
+    trainer.nnodes=1 \
+    trainer.save_freq=10 \
+    trainer.test_freq=-1 \
+    trainer.project_name=$WAND_PROJECT \
+    trainer.experiment_name=$EXPERIMENT_NAME \
+    trainer.total_epochs=15 \
+    trainer.total_training_steps=1005 \
+    trainer.default_hdfs_dir=null \
+    trainer.default_local_dir=/mnt/geminiszgmcephfs/geminicephfs/pr-others-prctrans/xinyanguan/Search-R1/verl_checkpoints/$EXPERIMENT_NAME \
+    max_turns=8 \
+    retriever.url="http://127.0.0.1:8000/retrieve" \
+    retriever.topk=3 \
+    2>&1 | tee $EXPERIMENT_NAME.log
