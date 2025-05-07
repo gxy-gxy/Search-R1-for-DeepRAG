54c06a2e4ec7093b408bfac1e638d6639e824467
xinyanguan <xinyanguan@tencent.com>
2025-05-07 19:38:33 +0800

    add bm25

--- search_r1/llm_agent/generation.py
+++ search_r1/llm_agent/generation.py
@@ -9,6 +9,30 @@ from verl import DataProto
 from verl.utils.tracking import Tracking
 import shutil
 import requests
+from typing import List, Dict, Tuple
+import os
+import time
+import tqdm
+import uuid
+import numpy as np
+import torch
+# import faiss
+import logging
+import pandas as pd
+from transformers import AutoTokenizer, AutoModel
+from beir.datasets.data_loader import GenericDataLoader
+from beir.retrieval.evaluation import EvaluateRetrieval
+from beir.retrieval.search.lexical import BM25Search
+from beir.retrieval.search.lexical.elastic_search import ElasticSearch
+
+logging.basicConfig(level=logging.INFO) 
+logger = logging.getLogger(__name__)
+
+def get_random_doc_id():
+    return f'_{uuid.uuid4()}'
+
+import os
+ES_HOST = os.getenv("ES_HOST", "localhost")
 
 @dataclass
 class GenerationConfig:
@@ -34,6 +58,7 @@ class LLMGenerationManager:
         self.actor_rollout_wg = actor_rollout_wg
         self.config = config
         self.is_validation = is_validation
+        self.retriever = BM25(tokenizer=self.tokenizer, index_name='wiki')
 
         self.tensor_fn = TensorHelper(TensorConfig(
             pad_token_id=tokenizer.pad_token_id,
@@ -194,7 +219,7 @@ class LLMGenerationManager:
             pad_sequence = v[0:1].repeat(padding_size, *[1] * (len(v.shape) - 1))
             padded_batch[k] = torch.cat([v, pad_sequence], dim=0)
 
-        padded_active_batch = DataProto.from_dict(padded_batch)
+        padded_active_batch = DataProto.from_dict(padded_batch, meta_info=active_batch.meta_info)
         for key in padded_active_batch.batch.keys():
             padded_active_batch.batch[key] = padded_active_batch.batch[key].long()
 
@@ -223,8 +248,8 @@ class LLMGenerationManager:
         original_left_side = {'input_ids': initial_input_ids[:, -self.config.max_start_length:]}
         original_right_side = {'responses': initial_input_ids[:, []], 'responses_with_info_mask': initial_input_ids[:, []]}
         
-        active_mask = torch.ones(gen_batch.batch['input_ids'].shape[0], dtype=torch.bool)
-        turns_stats = torch.ones(gen_batch.batch['input_ids'].shape[0], dtype=torch.int)
+        active_mask = torch.ones(gen_batch.batch['input_ids'].shape[0], dtype=torch.bool) # prompt对应的 response有没有结束
+        turns_stats = torch.ones(gen_batch.batch['input_ids'].shape[0], dtype=torch.int) # 每个prompt对应的response的turn数
         valid_action_stats = torch.zeros(gen_batch.batch['input_ids'].shape[0], dtype=torch.int)
         valid_search_stats = torch.zeros(gen_batch.batch['input_ids'].shape[0], dtype=torch.int)
         active_num_list = [active_mask.sum().item()]
@@ -232,6 +257,7 @@ class LLMGenerationManager:
 
         # Main generation loop
         for step in range(self.config.max_turns):
+            print(f"total {gen_batch.batch['input_ids'].shape[0]} responses, remaining {active_mask.sum().item()} responses")
             if not active_mask.sum():
                 break
             rollings.batch = self.tensor_fn.cut_to_effective_len(
@@ -242,7 +268,11 @@ class LLMGenerationManager:
             # gen_output = self.actor_rollout_wg.generate_sequences(rollings)
             rollings_active = DataProto.from_dict({
                 k: v[active_mask] for k, v in rollings.batch.items()
-            })            
+            })             
+            # 这个函数是为了dummy pad generate
+            # 生成follow up 
+            # TODO 加 stop str， let's search
+            rollings_active.meta_info['stop'] = ["Let's search"]
             gen_output = self._generate_with_gpu_padding(rollings_active)
 
             meta_info = gen_output.meta_info            
@@ -364,12 +394,15 @@ class LLMGenerationManager:
         Returns:
             List of observation strings
         """
+        # action：answer, search
+        # TODO action：retrieve/ no retrieve(final answer)
         cur_actions, contents = self.postprocess_predictions(predictions)
         next_obs, dones, valid_action, is_search = [], [], [], []
         
         search_queries = [content for action, content in zip(cur_actions, contents) if action == 'search']
         if do_search:
-            search_results = self.batch_search(search_queries)
+            # search_results = self.batch_search(search_queries)
+            search_results = self.batch_bm25_search(search_queries)
             assert len(search_results) == sum([1 for action in cur_actions if action == 'search'])
         else:
             search_results = [''] * sum([1 for action in cur_actions if action == 'search'])
@@ -388,17 +421,19 @@ class LLMGenerationManager:
                     valid_action.append(1)
                     is_search.append(0)
                 elif action == 'search':
-                    next_obs.append(f'\n\n<information>{search_results.pop(0).strip()}</information>\n\n')
+                    # next_obs.append(f'\n\n<information>{search_results.pop(0).strip()}</information>\n\n')
+                    next_obs.append(f" the question in Wikipedia.\n{search_results.pop(0).strip()}\nIntermediate answer:")
                     dones.append(0)
                     valid_action.append(1)
                     is_search.append(1)
                 else:
-                    next_obs.append(f'\nMy previous action is invalid. \
-If I want to search, I should put the query between <search> and </search>. \
-If I want to give the final answer, I should put the answer between <answer> and </answer>. Let me try again.\n')
-                    dones.append(0)
-                    valid_action.append(0)
-                    is_search.append(0)
+                    raise ValueError(f"Invalid action: {action}")
+#                     next_obs.append(f'\nMy previous action is invalid. \
+# If I want to search, I should put the query between <search> and </search>. \
+# If I want to give the final answer, I should put the answer between <answer> and </answer>. Let me try again.\n')
+#                     dones.append(0)
+#                     valid_action.append(0)
+#                     is_search.append(0)
             
         assert len(search_results) == 0
             
@@ -417,22 +452,32 @@ If I want to give the final answer, I should put the answer between <answer> and
         actions = []
         contents = []
                 
-        for prediction in predictions:
-            if isinstance(prediction, str): # for llm output
-                pattern = r'<(search|answer)>(.*?)</\1>'
-                match = re.search(pattern, prediction, re.DOTALL)
-                if match:
-                    content = match.group(2).strip()  # Return only the content inside the tags
-                    action = match.group(1)
-                else:
-                    content = ''
-                    action = None
+        # for prediction in predictions:
+        #     if isinstance(prediction, str): # for llm output
+        #         pattern = r'<(search|answer)>(.*?)</\1>'
+        #         match = re.search(pattern, prediction, re.DOTALL)
+        #         if match:
+        #             content = match.group(2).strip()  # Return only the content inside the tags
+        #             action = match.group(1)
+        #         else:
+        #             content = ''
+        #             action = None
+        #     else:
+        #         raise ValueError(f"Invalid prediction type: {type(prediction)}")
+            
+        #     actions.append(action)
+        #     contents.append(content)
+        
+        for pred in predictions:
+            pred = pred.strip()
+
+            if pred.endswith("Let's search"):
+                actions.append("search")
+                contents.append(pred.removesuffix("Let's search").strip().split("Follow up:")[-1].strip())  # Will be filled from Follow up
             else:
-                raise ValueError(f"Invalid prediction type: {type(prediction)}")
-            
-            actions.append(action)
-            contents.append(content)
-            
+                actions.append("answer")
+                contents.append(pred)
+                
         return actions, contents
 
     def batch_search(self, queries: List[str] = None) -> str:
@@ -467,3 +512,190 @@ If I want to give the final answer, I should put the answer between <answer> and
             format_reference += f"Doc {idx+1}(Title: {title}) {text}\n"
 
         return format_reference
+    
+    def batch_bm25_search(self, queries: List[str]):
+        _, docs_list = self.retriever.retrieve(queries, topk=self.config.topk)
+        
+        results = []
+        
+        for docs in docs_list:
+            context = "Context:\n"
+            for i, doc in enumerate(docs):
+                context += f"[{i+1}] {doc}\n"
+            results.append(context)
+        return results
+
+
+
+
+
+
+class BM25:
+    def __init__(
+        self,
+        tokenizer: AutoTokenizer = None,
+        index_name: str = None,
+        engine: str = 'elasticsearch',
+        **search_engine_kwargs,
+    ):
+        self.tokenizer = tokenizer
+        # load index
+        assert engine in {'elasticsearch', 'bing'}
+        if engine == 'elasticsearch':
+            self.max_ret_topk = 1000
+            self.retriever = EvaluateRetrieval(
+                BM25Search(index_name=index_name, hostname=ES_HOST, initialize=False, number_of_shards=1, maxsize=5),
+                k_values=[self.max_ret_topk])
+
+    def retrieve(
+        self,
+        queries: List[str],  # (bs,)
+        topk: int = 1,
+        max_query_length: int = None,
+    ):
+        assert topk <= self.max_ret_topk
+        device = None
+        bs = len(queries)
+
+        # truncate queries
+        if max_query_length:
+            ori_ps = self.tokenizer.padding_side
+            ori_ts = self.tokenizer.truncation_side
+            # truncate/pad on the left side
+            self.tokenizer.padding_side = 'left'
+            self.tokenizer.truncation_side = 'left'
+            tokenized = self.tokenizer(
+                queries,
+                truncation=True,
+                padding=True,
+                max_length=max_query_length,
+                add_special_tokens=False,
+                return_tensors='pt')['input_ids']
+            self.tokenizer.padding_side = ori_ps
+            self.tokenizer.truncation_side = ori_ts
+            queries = self.tokenizer.batch_decode(tokenized, skip_special_tokens=True)
+
+        # retrieve
+        results: Dict[str, Dict[str, Tuple[float, str]]] = self.retriever.retrieve(
+            None, dict(zip(range(len(queries)), queries)), disable_tqdm=True)
+
+        # prepare outputs
+        docids: List[str] = []
+        docs: List[str] = []
+        for qid, query in enumerate(queries):
+            _docids: List[str] = []
+            _docs: List[str] = []
+            if qid in results:
+                for did, (score, text) in results[qid].items():
+                    _docids.append(did)
+                    _docs.append(text)
+                    if len(_docids) >= topk:
+                        break
+            if len(_docids) < topk:  # add dummy docs
+                _docids += [get_random_doc_id() for _ in range(topk - len(_docids))]
+                _docs += [''] * (topk - len(_docs))
+            docids.extend(_docids)
+            docs.extend(_docs)
+
+        docids = np.array(docids).reshape(bs, topk)  # (bs, topk)
+        docs = np.array(docs).reshape(bs, topk)  # (bs, topk)
+        return docids, docs
+
+
+def bm25search_search(self, corpus: Dict[str, Dict[str, str]], queries: Dict[str, str], top_k: int, *args, **kwargs) -> Dict[str, Dict[str, float]]:
+    # Index the corpus within elastic-search
+    # False, if the corpus has been already indexed
+    if self.initialize:
+        self.index(corpus)
+        # Sleep for few seconds so that elastic-search indexes the docs properly
+        time.sleep(self.sleep_for)
+
+    #retrieve results from BM25
+    query_ids = list(queries.keys())
+    queries = [queries[qid] for qid in query_ids]
+
+    final_results: Dict[str, Dict[str, Tuple[float, str]]] = {}
+    for start_idx in tqdm.trange(0, len(queries), self.batch_size, desc='que', disable=kwargs.get('disable_tqdm', False)):
+        query_ids_batch = query_ids[start_idx:start_idx+self.batch_size]
+        results = self.es.lexical_multisearch(
+            texts=queries[start_idx:start_idx+self.batch_size],
+            top_hits=top_k)
+        for (query_id, hit) in zip(query_ids_batch, results):
+            scores = {}
+            for corpus_id, score, text in hit['hits']:
+                scores[corpus_id] = (score, text)
+                final_results[query_id] = scores
+
+    return final_results
+
+BM25Search.search = bm25search_search
+
+
+def elasticsearch_lexical_multisearch(self, texts: List[str], top_hits: int, skip: int = 0) -> Dict[str, object]:
+    """Multiple Query search in Elasticsearch
+
+    Args:
+        texts (List[str]): Multiple query texts
+        top_hits (int): top k hits to be retrieved
+        skip (int, optional): top hits to be skipped. Defaults to 0.
+
+    Returns:
+        Dict[str, object]: Hit results
+    """
+    request = []
+
+    assert skip + top_hits <= 10000, "Elastic-Search Window too large, Max-Size = 10000"
+
+    for text in texts:
+        req_head = {"index" : self.index_name, "search_type": "dfs_query_then_fetch"}
+        req_body = {
+            "_source": True, # No need to return source objects
+            "query": {
+                "multi_match": {
+                    "query": text, # matching query with both text and title fields
+                    "type": "best_fields",
+                    "fields": [self.title_key, self.text_key],
+                    "tie_breaker": 0.5
+                    }
+                },
+            "size": skip + top_hits, # The same paragraph will occur in results
+            }
+        request.extend([req_head, req_body])
+
+    res = self.es.msearch(body = request)
+
+    result = []
+    for resp in res["responses"]:
+        responses = resp["hits"]["hits"][skip:] if 'hits' in resp else []
+
+        hits = []
+        for hit in responses:
+            hits.append((hit["_id"], hit['_score'],f"Title: {hit['_source']['title']} Text: {hit['_source']['txt']}"))
+
+        result.append(self.hit_template(es_res=resp, hits=hits))
+    return result
+
+ElasticSearch.lexical_multisearch = elasticsearch_lexical_multisearch
+
+
+def elasticsearch_hit_template(self, es_res: Dict[str, object], hits: List[Tuple[str, float]]) -> Dict[str, object]:
+    """Hit output results template
+
+    Args:
+        es_res (Dict[str, object]): Elasticsearch response
+        hits (List[Tuple[str, float]]): Hits from Elasticsearch
+
+    Returns:
+        Dict[str, object]: Hit results
+    """
+    result = {
+        'meta': {
+            'total': es_res['hits']['total']['value'] if 'hits' in es_res else None,
+            'took': es_res['took'] if 'took' in es_res else None,
+            'num_hits': len(hits)
+        },
+        'hits': hits,
+    }
+    return result
+
+ElasticSearch.hit_template = elasticsearch_hit_template
